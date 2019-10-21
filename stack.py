e6e892b22ac0ff8a01abf5502e46f56abacb709c
Kristen Orue <korue113@gmail.com>
2019-10-20 22:29:42 -0700

    Passed three Tests

---
+++ stack.py
@@ -0,0 +1,21 @@
+# Stack: A stack.
+# Your implementation should pass the tests in stack.py.
+# YOUR NAME
+
+class Stack:
+
+    def __init__(self):
+        self.items = []
+        
+    def is_empty(self):
+        return self.items == []
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items)-1]
+ 
+
+
+ 
